6ca8a21d703f609c6b4fa949e5c4cbf07845c8f9
ayomikunbot-create <ayomikunbot@gmail.com>
2026-06-14 01:19:33 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "@tanstack/react-router";
-import { QueryClient } from "@tanstack/react-query";
+import { createRoot } from "react-dom/client";
 
-import { router } from "./router";
-
-const queryClient = new QueryClient();
-
-// 👇 VERY IMPORTANT: give router its context
-router.update({
-  context: {
-    queryClient,
-  },
-});
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <h1>Ayomikun TV is working</h1>
 );
