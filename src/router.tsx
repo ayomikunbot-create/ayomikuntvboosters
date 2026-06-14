42ee822e09e08162b3695bd3dc50c3fc026ed1f2
ayomikunbot-create <ayomikunbot@gmail.com>
2026-06-14 01:34:31 +0100

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,10 @@
-import { createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { QueryClient } from "@tanstack/react-query";
 
 export const router = createRouter({
   routeTree,
-})
+  context: {
+    queryClient: undefined! as QueryClient,
+  },
+});
